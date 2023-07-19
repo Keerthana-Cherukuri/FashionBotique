fa828f4c0f7a597f9fa6b4784149da7aaa07ddad
Keerthana-Cherukuri <102681496+Keerthana-Cherukuri@users.noreply.github.com>
2023-07-19 11:10:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navigator');
+const cl = document.getElementById('close');
+
+if(bar)
+{
+	bar.addEventListener('click',()=>{
+		nav.classList.add('active');
+	})
+}
+if(cl)
+{
+	cl.addEventListener('click',()=>{
+		nav.classList.remove('active');
+	})
+}
